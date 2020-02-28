4fd81b68fe1ef6dd8519ac30d7c86d5e33590655
jinyang <kylin27@126.com>
2020-02-28 15:12:57 +0800

    生成api文档

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "quickstart/routers"
 	"github.com/astaxie/beego"
+
 )
 
 func main() {
--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,6 +7,24 @@ import (
 
 func init() {
 
+    beego.GlobalControllerRouter["quickstart/controllers:CMSController"] = append(beego.GlobalControllerRouter["quickstart/controllers:CMSController"],
+        beego.ControllerComments{
+            Method: "Product",
+            Router: `/products`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["quickstart/controllers:CMSController"] = append(beego.GlobalControllerRouter["quickstart/controllers:CMSController"],
+        beego.ControllerComments{
+            Method: "StaticBlock",
+            Router: `/staticblock/:key`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
     beego.GlobalControllerRouter["quickstart/controllers:MainController"] = append(beego.GlobalControllerRouter["quickstart/controllers:MainController"],
         beego.ControllerComments{
             Method: "JudgeLogin",
